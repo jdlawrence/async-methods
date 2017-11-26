64d93ccc5add157142ebacc351cb1c191ede5cc8
Jamil Lawrence <jamil.lawrence@gmail.com>
2017-11-25 19:43:25 -0800

    Display "callback hell example"

--- code.js
+++ code.js
@@ -5,13 +5,31 @@ function getJSON(cb) {
 
   let time = Math.random();
   fakeJSON.time = time;
+
   setTimeout(() => {
     cb(JSON.stringify(fakeJSON)); 
   }, time);
 }
-
+let results = [];
 const callback = (data) => {
-  console.log(`data is ${data}`);
+  // console.log(`data is ${data}`);
+  results.push(data);
+  console.log('results are', results);
 };
 
-getJSON(callback);
+getJSON((data) => {
+  callback(data);
+  getJSON((data) => {
+    callback(data);
+    getJSON((data) => {
+      callback(data);
+      getJSON((data) => {
+        callback(data);
+        getJSON((data) => {
+          callback(data);
+          getJSON(callback);
+        });
+      });
+    });
+  });
+});
