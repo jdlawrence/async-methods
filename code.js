9b1db5b09cc8624b4e21d34a5732ab1e2500cb7f
Jamil Lawrence <jamil.lawrence@gmail.com>
2017-11-30 20:01:37 -0800

    Finished async/await and generators

--- code.js
+++ code.js
@@ -51,24 +51,63 @@ function getJSONPromise() {
   });
 }
 
-getJSONPromise().then(data => {
-  callback(data);
-  return getJSONPromise();
-})
-.then(data => {
-  callback(data);
-  return getJSONPromise();
-})
-.then(data => {
-  callback(data);
-  return getJSONPromise();
-})
-.then(data => {
-  callback(data);
-  return getJSONPromise();
-})
-.then(data => {
-  callback(data);
-  return getJSONPromise();
-});
-  
+// getJSONPromise().then(data => {
+//   callback(data);
+//   return getJSONPromise();
+// })
+// .then(data => {
+//   callback(data);
+//   return getJSONPromise();
+// })
+// .then(data => {
+//   callback(data);
+//   return getJSONPromise();
+// })
+// .then(data => {
+//   callback(data);
+//   return getJSONPromise();
+// })
+// .then(data => {
+//   callback(data);
+//   return getJSONPromise();
+// });
+
+async function demonstrateAsync() {
+  let data;
+
+  for (let i = 0; i < 5; i++) {
+    data = await getJSONPromise();
+    console.log(data);
+    results.push(data);
+  }
+  console.log('results', results);
+}
+// demonstrateAsync();
+
+
+function getJSONGenerator(cb) {
+  let fakeJSON = {
+    "Jamil": "cool guy"
+  };
+
+  let time = Math.random();
+  fakeJSON.time = time;
+
+  setTimeout(() => {
+    it.next(JSON.stringify(fakeJSON));
+  }, time);
+}
+
+function *demonstrateGenerator() {
+  let data;
+
+  for (let i = 0; i < 5; i++) {
+    data = yield getJSONGenerator();
+    console.log('data', data);
+    results.push(data);
+  }
+  console.log('results', results);
+}
+
+let it = demonstrateGenerator();
+it.next();
