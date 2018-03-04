018f27df3676848160c40589d1879cc354d0cfc9
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-03-04 12:35:35 -0800

    Update code to use logData callback in all methods

--- code.js
+++ code.js
@@ -1,4 +1,23 @@
 function getJSON(cb) {
+  // Our JSON object to be passed to the callback
+  let fakeJSON = {
+    "Jamil": "cool guy"
+  };
+
+  // Simulating some random amount of time to completion for our asynchronous function
+  // Also we add the time to the fakeJSON object so that we can differentiate
+  // each invocation
+  let time = Math.random();
+  fakeJSON.time = time;
+
+  setTimeout(() => {
+    cb(JSON.stringify(fakeJSON));
+  }, time);
+}
+
+
+
+function getJSON(cb) {
   let fakeJSON = {
     "Jamil": "cool guy"
   };
@@ -11,30 +30,56 @@ function getJSON(cb) {
   }, time);
 }
 
-let results = [];
-const callback = (data) => {
-  results.push(data);
-  if (results.length >= 5) {
-    console.log('results are', results);
-  }
+
+
+
+// createLogger returns a function with clojure over the results array
+// When the returned function has been called five times, we log the result
+const createLogger = () => {
+  let results = [];
+
+  return (data) => {
+    results.push(data);
+    if (results.length >= 5) {
+      console.log('results are', results);
+    }
+  };
 };
 
+const logData = createLogger();
+
+// Logs something like:
+/*
+results are [ '{"Jamil":"cool guy","time":0.833740596226406}',
+'{"Jamil":"cool guy","time":0.8789151526112358}',
+'{"Jamil":"cool guy","time":0.019798040543230533}',
+'{"Jamil":"cool guy","time":0.4729462050942326}',
+'{"Jamil":"cool guy","time":0.9870582160353376}',
+'{"Jamil":"cool guy","time":0.4860649104312529}' ]
+*/
+
+
+
+
+
 // getJSON((data) => {
-//   callback(data);
+//   logData(data);
 //   getJSON((data) => {
-//     callback(data);
+//     logData(data);
 //     getJSON((data) => {
-//       callback(data);
+//       logData(data);
 //       getJSON((data) => {
-//         callback(data);
+//         logData(data);
 //         getJSON((data) => {
-//           callback(data);
-//           getJSON(callback);
+//           logData(data);
 //         });
 //       });
 //     });
 //   });
 // });
+
+
+
 
 function getJSONPromise() {
   return new Promise((resolve, reject) => {
@@ -51,41 +96,74 @@ function getJSONPromise() {
   });
 }
 
-// getJSONPromise().then(data => {
-//   callback(data);
-//   return getJSONPromise();
-// })
-// .then(data => {
-//   callback(data);
-//   return getJSONPromise();
-// })
-// .then(data => {
-//   callback(data);
-//   return getJSONPromise();
-// })
-// .then(data => {
-//   callback(data);
-//   return getJSONPromise();
-// })
-// .then(data => {
-//   callback(data);
-//   return getJSONPromise();
-// });
+function callLogDataPromise(data) {
+  logData(data);
+  return getJSONPromise();
+}
 
-async function demonstrateAsync() {
-  let data;
+getJSONPromise()
+  .then(callLogDataPromise)
+  .then(callLogDataPromise)
+  .then(callLogDataPromise)
+  .then(callLogDataPromise)
+  .then(callLogDataPromise);
+
+// // Or with a loop:
+let callPromise = getJSONPromise();
+
+for(let i = 0; i < 5; i++) {
+  callPromise.then(callLogDataPromise);
+}
+
+
+
+
+function getJSONPromiseAll() {
+  let promiseArr = [];
 
   for (let i = 0; i < 5; i++) {
-    data = await getJSONPromise();
-    console.log(data);
-    results.push(data);
+    promiseArr.push(getJSONPromise());
   }
-  console.log('results', results);
+  Promise.all(promiseArr).then(values => {
+    console.log(values);
+  });
 }
-// demonstrateAsync();
+
+// getJSONPromiseAll();
 
 
-function getJSONGenerator(cb) {
+
+async function getJSONAsync(cb) {
+  let data = await getJSONPromise();
+  cb(data);
+}
+
+
+getJSONAsync(logData);
+getJSONAsync(logData);
+getJSONAsync(logData);
+getJSONAsync(logData);
+getJSONAsync(logData);
+
+// Or, with a loop:
+for (let i = 0; i < 5; i++) {
+  getJSONAsync(logData);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getJSONGenerator(gen, cb) {
   let fakeJSON = {
     "Jamil": "cool guy"
   };
@@ -94,20 +172,43 @@ function getJSONGenerator(cb) {
   fakeJSON.time = time;
 
   setTimeout(() => {
-    it.next(JSON.stringify(fakeJSON));
+    const it = gen(JSON.stringify(fakeJSON));
+    cb(it.next().value);
   }, time);
 }
 
-function *demonstrateGenerator() {
-  let data;
 
-  for (let i = 0; i < 5; i++) {
-    data = yield getJSONGenerator();
-    console.log('data', data);
-    results.push(data);
-  }
-  console.log('results', results);
+function* gen(val) {
+  yield val;
 }
 
-let it = demonstrateGenerator();
-it.next();
+// getJSONGenerator(gen, logData);
+// getJSONGenerator(gen, logData);
+// getJSONGenerator(gen, logData);
+// getJSONGenerator(gen, logData);
+// getJSONGenerator(gen, logData);
+
+// Or, with a loop:
+for (let i = 0; i < 5; i++) {
+  getJSONGenerator(gen, logData);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
